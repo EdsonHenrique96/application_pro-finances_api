44e628a089fbdf09a949ba69ef563fe67a5fa49d
Edson Batista <edson.batista@catho.com>
2020-08-04 23:53:46 -0300

    feat(transactions): add route to delete a transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
 // import AppError from '../errors/AppError';
 
+import { getRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
+
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+    const transactionExists = await transactionRepository.findOne(id);
+
+    if (!transactionExists) {
+      throw new Error(`Transaction ${id} does not exist.`);
+    }
+
+    await transactionRepository.delete(id);
   }
 }
 
